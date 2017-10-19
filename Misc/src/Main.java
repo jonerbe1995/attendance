9664354005c4336a9980a0ee40732fbd01fe02c6
Jonathan Erbe <jonerbe1995@gmail.com>
2017-10-19 11:20:01 -0500

    Initial commit
    The first commit

---
+++ Misc/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * The attendance program for the Software Development Club of UW-Madison
+ * Made with love by the VP, Jon Erbe
+ * Be sure to commit this to your own, remote copy of master!
+ */
+class Attendance {
+    public static void main(String[] args) {
+        //Output your name and this week's word (ex: Jon Erbe chili-pepper)
+        System.out.print("");
+    }
+}
