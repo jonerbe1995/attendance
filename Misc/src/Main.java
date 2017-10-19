31a77934f5e5f6743a96611e0c5c0e363af71cfe
Jonathan Erbe <jonerbe1995@gmail.com>
2017-10-19 14:07:01 -0500

    2nd Commit

--- Misc/src/Main.java
+++ Misc/src/Main.java
@@ -1,11 +1,28 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  * The attendance program for the Software Development Club of UW-Madison
  * Made with love by the VP, Jon Erbe
  * Be sure to commit this to your own, remote copy of master!
  */
 class Attendance {
-    public static void main(String[] args) {
-        //Output your name and this week's word (ex: Jon Erbe chili-pepper)
-        System.out.print("");
+    private static String name = "", word = "", total = name + " " + word;
+    private static final String FILENAME = name + ".txt";
+
+    public static void main(String[] args) throws IOException {
+        //TODO: Fill your name and this week's word (ex: name = "Jon Erbe", word = "chili-pepper")
+        name = "";
+        word = "";
+
+        alg(total);
+    }
+
+    public static String alg(String file) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(FILENAME));
+        writer.write(file);
+        writer.close();
+        return file;
     }
 }
